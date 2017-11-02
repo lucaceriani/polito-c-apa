8837d478b8683f71551867c9179f05b4c5e958d7
Luca Ceriani <luca.ceri@gmail.com>
2017-11-02 22:04:41 +0100

    finished lab04_es2

--- lab04_es2/main.c
+++ lab04_es2/main.c
@@ -10,18 +10,19 @@
 #define MAXP 1000
 #define MAX_LUNG_PAROLA 20
 
-#define ever ;;
+#define non_strutturato ;;
 
 int leggiPagina(char [MAXR][MAXC+1], int);
 int riconosciParole(char [MAXR][MAXC+1], int, char *[MAXP], int);
 int confrontaParole(char*, char*); // accetta gli indici della parole nel vettore $parole
 void ordinaParole(char*[MAXP], int);
-void stampa(int, char*parole[MAXP]);
 int ricercaBinaria(char *[MAXP], int, char[MAX_LUNG_PAROLA]);
+struct pos convertiIndice(char*, char[MAXR][MAXC]);
 void minuscola(char*);
+void stampa(int, char*[MAXP]); // funzione utile per il debug
 
 struct pos {
-	int x,y;
+    int r,c;
 };
 
 int main() {
@@ -29,28 +30,21 @@ int main() {
     char pagina[MAXR][MAXC+1];
     char *parole[MAXP];
     char cerca[MAX_LUNG_PAROLA+1];
-	int nr, np, i, indice;
+    int nr, np, i, indice;
+    struct pos p;
 
-	nr=leggiPagina(pagina, MAXR);
-	printf("Letto il file \"%s\"\n", NOME_FILE);
-	printf("Trovat%s %d rig%s\n", nr==1?"a":"e", nr, nr==1?"a":"he");
+    nr=leggiPagina(pagina, MAXR);
+    printf("Letto il file \"%s\"\n", NOME_FILE);
+    printf("Trovat%s %d rig%s\n", nr==1?"a":"e", nr, nr==1?"a":"he");
 
     np=riconosciParole(pagina, nr, parole, MAXP);
     printf("Trovat%s %d parol%s\n", np==1?"o":"e", np, np==1?"a":"e");
 
-	ordinaParole(parole, np);
+    ordinaParole(parole, np);
 
-	printf("**%d**\n", confrontaParole("ciao ", "ciao,"));
+    puts("\nInserisci la parola da cercare:");
 
-	for (i=0; i<np; i++){
-		printf("%d -> ", i);
-		stampa(i,parole);
-		printf("\n");
-	}
-	puts("");
-
-	puts("Inserisci la parola da cercare:");
-    for(ever) {
+    for(non_strutturato) {
         printf("> ");
         fgets(cerca, MAX_LUNG_PAROLA+1, stdin);
         sscanf(cerca, "%s", cerca);
@@ -60,10 +54,28 @@ int main() {
         if (strcmp(cerca, "$fine")==0) exit(0);
 
         indice=ricercaBinaria(parole, np, cerca);
-		printf("indice parola \"%s\": %d\n", cerca, indice); // non funziona adesso
 
-		// to do: - ricerca in avanti o all'indietro dei duplicati
-		//        - conversione indice -> coordinate
+        // prima di fare la converisone dell'indice, per evitare errori
+        // Ã¨ meglio controllare che l'indice ritornato non sia -1 e in
+        // tal caso, uscire
+        if (indice==-1) {
+            printf("Parola non trovata\n");
+            continue;
+        }
+
+        printf("Parola \"%s\" trovata in (%d,%d) ", cerca, convertiIndice(parole[indice], pagina));
+
+        // ricerca avanti e indietro delle parole uguali
+        i=indice;
+        while (confrontaParole(cerca, parole[++i])==0) {
+            printf("(%d,%d) ", convertiIndice(parole[i], pagina));
+
+        }
+        i=indice;
+        while (confrontaParole(cerca, parole[--i])==0) {
+            printf("(%d,%d) ", convertiIndice(parole[i], pagina));
+        }
+        puts("");
 
     }
 
@@ -72,41 +84,39 @@ int main() {
 
 int leggiPagina(char pagina[MAXR][MAXC+1], int max_r) {
 
-	FILE *fp;
-	int i=0;
+    FILE *fp;
+    int i=0;
 
     if ((fp=fopen(NOME_FILE, "r"))==NULL) exit(1);
     //lettura riga per riga
     while (fgets(pagina[i++], MAXC+1, fp)!=NULL && i<max_r);
-	fclose(fp);
-	return i-1;
+    fclose(fp);
+    return i-1;
 }
 
 int riconosciParole(char pagina[MAXR][MAXC+1], int nr, char *parole[MAXP], int max_p) {
 
     int in_parola=0;
-	int n_parole=0;
-	int i,j, lung, p_index=0;
+    int n_parole=0;
+    int i,j, p_index=0;
 
     for (i=0; i<nr; i++) {
-		// mi tolgo via i caratteri che non mi servono
+        for (j=0; pagina[i][j] && n_parole<max_p; j++) {
+            if (isalpha(pagina[i][j])==0 && in_parola==1) {
+                 n_parole++;
+                in_parola=0;
 
-		for (j=0; pagina[i][j] && n_parole<max_p; j++) {
-			if (isalpha(pagina[i][j])==0 && in_parola==1) {
-			 	n_parole++;
-				in_parola=0;
-
-			} else if (isalpha(pagina[i][j])) {
-				pagina[i][j]=tolower(pagina[i][j]); // considero solo minuscole
-				if (in_parola==0) {
-					// sono dentro una parola nuova
-					// mi salvo il puntatore
-					in_parola=1; // entro nella parola
-					parole[p_index]=&pagina[i][j]; // salvo il puntatore alla parola
-					p_index++; // incremento l'indice del puntatore
-				}
-			}
-		}
+            } else if (isalpha(pagina[i][j])) {
+                pagina[i][j]=tolower(pagina[i][j]); // considero solo minuscole
+                if (in_parola==0) {
+                    // sono dentro una parola nuova
+                    // mi salvo il puntatore
+                    parole[p_index]=&pagina[i][j]; // salvo il puntatore alla parola
+                    p_index++; // incremento l'indice del puntatore
+                    in_parola=1; // entro nella parola
+                }
+            }
+        }
     }
 
     return n_parole;
@@ -114,36 +124,36 @@ int riconosciParole(char pagina[MAXR][MAXC+1], int nr, char *parole[MAXP], int m
 
 int confrontaParole(char *a, char *b) {
 
-	int i=0;
+    int i=0;
 
     while(isalpha(a[i]) && isalpha(b[i])) {
-		if (a[i]!=b[i]) {
-			return a[i]<b[i];
-		} else {
-			i++;
-		}
+        if (a[i]!=b[i]) {
+            return (a[i]<b[i])?1:-1;
+        } else {
+            i++;
+        }
     }
 
     // se le due parole sono uguali fino alla fine ma una finisce prima,
-    // in ordine alfabetico viene prima quella più corta
+    // in ordine alfabetico viene prima quella piÃ¹ corta
 
-    //se $a è la stringa più lunga...
+    //se $a Ã¨ la stringa piÃ¹ lunga...
     if (isalpha(a[i]) && isalpha(b[i])==0) {
-		return -1; //n.b. $i è già aumentato di uno
+        return -1; //n.b. $i Ã¨ giÃ  aumentato di uno
     } else if (isalpha(a[i])==0 && isalpha(b[i])==0) {
-    	return 0;
-	} else {
-		return 1; // altrimeti le parole sono in ordine
-	}
-	//  1 = sono in ordine
-	//  0 = sono uguali
-	// -1 = non sono in ordine
+        return 0;
+    } else {
+        return 1; // altrimenti le parole sono in ordine
+    }
+    //  1 = sono in ordine
+    //  0 = sono uguali
+    // -1 = non sono in ordine
 
 }
 
 void ordinaParole(char *parole[MAXP], int np) {
 
-	// classico insertion sort
+    // classico insertion sort
 
     int i,j;
     char *x;
@@ -163,33 +173,42 @@ int ricercaBinaria(char *parole[MAXP], int np, char *cerca) {
 
     int l,r,m;
     l=0; r=np-1;
-    while (l<=r) {
-		m=(l+r)/2;
-		stampa(m, parole);
-        if (confrontaParole(cerca, parole[m])==1){ // proseguo nel sottovettore sx
-            r=m-1;
-        } else if (confrontaParole(cerca, parole[m])==-1) { // proseguo nel sottovettore dx
+    while ((r-l)!=0) {
+        m=(l+r)/2;
+        if (confrontaParole(cerca, parole[m])==0) {
+            return m;
+        } else if (confrontaParole(cerca, parole[m])==1){ // proseguo nel sottovettore sx
+            r=m;
+        } else { // proseguo nel sottovettore dx
             l=m+1;
-		} else {
-			puts("");
-			return m;
-		}
+        }
     }
-
-	return -1;
+    if (confrontaParole(cerca, parole[l])==0)
+        return l;
+    else
+        return -1;
 }
 
+struct pos convertiIndice(char *parola, char pagina[MAXR][MAXC]) {
+    // attenzione! parola Ã¨ giÃ  il puntatore alla parola,
+    //             non Ã¨ il vettore *parole[MAXP]
+    struct pos p;
+    p.r=(parola-pagina[0])/MAXC; //trovo la riga
+    p.c=(parola-pagina[p.r]);    //trovo la colonna
+    return p;
+};
+
 void stampa(int i, char *parole[MAXP]) {
-	int j=0;
+    // stampa la parola dato l'indice i
+    int j=0;
     while(isalpha(parole[i][j])) {
-		printf("%c", parole[i][j++]);
+        printf("%c", parole[i][j++]);
     }
     printf(" ");
 }
 
-
 void minuscola(char *s) {
-	for (;*s;++s) *s=tolower(*s);
+    for (;*s;++s) *s=tolower(*s);
 }
 
 // fine
